6002f2a25b8d61ee420d4ff3139588959b065e77
xNekom <pedrojuuu@gmail.com>
2024-02-13 23:58:33 +0100

    Format

--- Web/Calculadora/script.js
+++ Web/Calculadora/script.js
@@ -1,45 +1,45 @@
-let display = document.getElementById('display');
-let currentNumber = '';
+let display = document.getElementById("display");
+let currentNumber = "";
 let operator = null;
 let firstOperand = null;
 let waitingForSecondOperand = false;
 
 function appendNumber(number) {
-    if (waitingForSecondOperand) {
-        display.value = number;
-        waitingForSecondOperand = false;
-    } else {
-        display.value = display.value + number;
-    }
+  if (waitingForSecondOperand) {
+    display.value = number;
+    waitingForSecondOperand = false;
+  } else {
+    display.value = display.value + number;
+  }
 }
 
 function appendOperator(op) {
-    if (operator !== null) calculate();
-    firstOperand = parseFloat(display.value);
-    operator = op;
-    waitingForSecondOperand = true;
+  if (operator !== null) calculate();
+  firstOperand = parseFloat(display.value);
+  operator = op;
+  waitingForSecondOperand = true;
 }
 
 function calculate() {
-    let secondOperand = parseFloat(display.value);
-    switch (operator) {
-        case '+':
-            display.value = firstOperand + secondOperand;
-            break;
-        case '-':
-            display.value = firstOperand - secondOperand;
-            break;
-        case '*':
-            display.value = firstOperand * secondOperand;
-            break;
-        case '/':
-            display.value = firstOperand / secondOperand;
-            break;
-    }
-    operator = null;
-    waitingForSecondOperand = false;
+  let secondOperand = parseFloat(display.value);
+  switch (operator) {
+    case "+":
+      display.value = firstOperand + secondOperand;
+      break;
+    case "-":
+      display.value = firstOperand - secondOperand;
+      break;
+    case "*":
+      display.value = firstOperand * secondOperand;
+      break;
+    case "/":
+      display.value = firstOperand / secondOperand;
+      break;
+  }
+  operator = null;
+  waitingForSecondOperand = false;
 }
 
 function clearDisplay() {
-    display.value = '';
+  display.value = "";
 }
